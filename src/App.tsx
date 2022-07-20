115cb8f22b12ccf2cae0484c8bd5acc2c1af602f
noname <48761044+noname0310@users.noreply.github.com>
2022-07-20 16:56:21 +0900

    clean project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import "./App.css";
+import styled from "styled-components";
 
-import logo from "./logo.svg";
+const ContainerDiv = styled.div`
+    margin: 0;
+    min-height: 100vh;
+`;
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-          Learn React
-                </a>
-            </header>
-        </div>
+        <ContainerDiv>
+        </ContainerDiv>
     );
 }
 
