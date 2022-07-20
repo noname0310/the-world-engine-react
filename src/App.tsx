0c5a04714c1fb7555a2015bc9079a750c10ab655
noname <48761044+noname0310@users.noreply.github.com>
2022-07-20 17:20:25 +0900

    add game

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import { Game } from "the-world-engine";
+
+import { Bootstrapper } from "./asset/Bootstrapper";
 
 const ContainerDiv = styled.div`
     margin: 0;
@@ -6,8 +10,31 @@ const ContainerDiv = styled.div`
 `;
 
 function App(): JSX.Element {
+    const [game, setGame] = useState<Game|null>(null);
+    const gameContainer = useRef<HTMLDivElement>(null);
+
+    useEffect((): (() => void)|void => {
+        if (!gameContainer.current) return;
+        if (game || false) return;
+
+        console.log("Initializing game...");
+
+        const newGame = new Game(gameContainer.current);
+        setGame(newGame);
+        newGame.run(Bootstrapper);
+        newGame.inputHandler.startHandleEvents();
+        
+        return (): void => {
+            console.log("Shutting down game...");
+
+            newGame.inputHandler.stopHandleEvents();
+            newGame.dispose();
+            setGame(null);
+        };
+    }, [gameContainer]);
+
     return (
-        <ContainerDiv>
+        <ContainerDiv ref={gameContainer}>
         </ContainerDiv>
     );
 }
